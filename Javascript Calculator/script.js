a0bf06b673c7ec46a99e0ff94fd0a5291a607896
AceDev28 <78961552+AceDev28@users.noreply.github.com>
2021-05-21 15:08:26 +0700

    Add files via upload
    - Change button word to key
    - Change calculator-normal-key to calculator-key
    - Add last command field ( show the last number and operant )
    - Add operant-key class
    - Change number key color to orange

---
+++ Javascript Calculator/script.js
@@ -0,0 +1,11 @@
+// holder
+// const calculator = document.querySelector('.calculator');
+
+// const keys = calculator.querySelector('.calculator_keys');
+
+// keys.addEventListener('click', e => { if (e.target.matches('button'))
+//     {
+//         // do things
+//     }
+
+// })
