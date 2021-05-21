49638076538e4fac7f2605bb872ab85fafd72428
AceDev28 <78961552+AceDev28@users.noreply.github.com>
2021-05-21 16:19:29 +0700

    Cleaning HTML
    - remove leftover class from the html calculator key
    - add data-action to operant
    - change anchor to button
    - delete div inside button key ( fix overlapping )
    - clean whitespace

--- Javascript Calculator/script.js
+++ Javascript Calculator/script.js
@@ -1,11 +1,50 @@
-// holder
-// const calculator = document.querySelector('.calculator');
 
-// const keys = calculator.querySelector('.calculator_keys');
+const calculator = document.querySelector('#main-cal-body')
 
-// keys.addEventListener('click', e => { if (e.target.matches('button'))
-//     {
-//         // do things
-//     }
+const keys = calculator.querySelector('#calculator-key-grid')
 
-// })
+keys.addEventListener('click',
+    // function
+    e => 
+    { if (e.target.matches('button'))
+        {
+            // do things
+                        //
+            const key = e.target
+            const action = key.dataset.action
+
+            if(!action)
+            {
+                console.log("This is a number key")
+            }
+
+            if(
+                action ==='add'     ||
+                action ==='subtract'||
+                action ==='multiply'||
+                action ==='divide'
+            )
+            {
+                console.log("This is an operant key")
+            }
+
+            if( action === 'decimal')
+            {
+                console.log("This is a decimal key")
+            }
+
+            if( action === 'calculate' )
+            {
+                console.log("This is an equal key")
+            }
+
+            if( action === 'delete' )
+            {
+                console.log("This is a delete key")
+            }
+        }
+
+    }
+)
+
+
